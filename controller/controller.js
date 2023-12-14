a52d977189782975ef973d39c374f78ce2e90868
nurammarnaufal <nurammarnaufal@gmail.com>
2023-12-14 10:44:25 +0700

    .

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,13 @@
-async function reqToken(req, res) {}
+async function reqToken(req, res) {
+
+}
+
 
 async function inputData(req, res) {
   const { token, volume, debit } = req.body;
 
-  res.json({ msg: "masuk mas" });
 }
 
-module.exports = { inputData, reqToken };
+async function dashboard(req, res) {}
+
+module.exports = { inputData, reqToken, dashboard };
--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.get("/", (req, res) => {
   res.json("Halo 👋, ini server smart valve");
 });
 
-const { inputData, reqToken } = require("./controller/controller");
-app.get("/cek", inputData);
+const { inputData, reqToken, dashboard } = require("./controller/controller");
+app.post("/api/sv1/req-token", reqToken);
+app.post("/api/sv1/input-data", inputData);
+app.post("/api/sv1/dashboard", dashboard);
 
 app.listen(8080, () => {
   console.log("server running on port 8080");
